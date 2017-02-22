37e06db20b9805fc9433f8d43667d3a93a1b4fe3
Pedro Luis Rivera <pedro.rivera40@upr.edu>
2017-02-22 00:29:54 -0400

    Adding VirtualDiskBlock class (Methods declaration) - Pedro Luis Rivera

---
+++ P1_4035_802146446/src/P1/VirtualDiskBlock.java
@@ -0,0 +1,24 @@
+package P1;
+
+public class VirtualDiskBlock {
+	
+	public VirtualDiskBlock() {
+		
+	}
+	
+	public VirtualDiskBlock(int blockCapacity) {
+		
+	}
+	
+	public int getCapacity() {
+		
+	}
+	
+	public void setElement(int index, byte newElement) {
+		
+	}
+	
+	public byte getElement(int index) {
+		
+	}
+}
